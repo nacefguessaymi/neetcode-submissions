03c8b1acce1a97a473970930eaa9591e06aede3b
Nacef Guessaymi <140124079+nacefguessaymi@users.noreply.github.com>
2026-06-05 14:18:42 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_s = s.replace(" ", "").replace("?", "").replace(".", "").replace("!", "").replace(",", "").replace("'", "").replace(":", "").lower()
+        mid = len(new_s) // 2
+        for i, c in enumerate(new_s[:mid]):
+            if c != new_s[-(i + 1)]:
+                return False
+        return True
